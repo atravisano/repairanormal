2ebe3028e5d8281ec97ef5f2ee9ac7378d89ac42
John Schlanger <john.a.schlanger@gmail.com>
2020-02-01 14:03:55 -0500

    Create power indicator

---
+++ Assets/Scaling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    public GameObject indicator;
+    private Transform indicatorCylinder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.localScale.y >= 0.0f)
+            this.transform.localScale -= new Vector3(0,.001f,0);
+    }
+}
