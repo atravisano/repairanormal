17b9557c310b73af22356c8111d787c4222ceb7b
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2020-02-01 13:53:31 -0500

    Enable spark when not collided

--- Assets/Scripts/AttachCableOnCollide.cs
+++ Assets/Scripts/AttachCableOnCollide.cs
@@ -19,18 +19,26 @@ public class AttachCableOnCollide : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == GameObjectName)
+        {
+            // disable spark
+            EnableSpark(false);
+
+            // creates joint
+            FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+            // sets joint position to point of contact
+            joint.anchor = col.contacts[0].point;
+            // conects the joint to the other object
+            joint.connectedBody = col.contacts[0].otherCollider.transform.GetComponentInParent<Rigidbody>();
+            // Stops objects from continuing to collide and creating more joints
+            joint.enableCollision = false;
+            Debug.Log("Connected!");
+        }
+    }
+    
+     void EnableSpark(bool isEnabled)
      {
-         if (col.gameObject.name == GameObjectName)
-         {
-        // creates joint
-         FixedJoint joint = gameObject.AddComponent<FixedJoint>(); 
-        // sets joint position to point of contact
-         joint.anchor = col.contacts[0].point; 
-        // conects the joint to the other object
-         joint.connectedBody = col.contacts[0].otherCollider.transform.GetComponentInParent<Rigidbody>(); 
-        // Stops objects from continuing to collide and creating more joints
-         joint.enableCollision = false;
-        Debug.Log("Connected!");
-         }
+        this.transform.GetChild(0).gameObject.SetActive(isEnabled);
      }
 }
