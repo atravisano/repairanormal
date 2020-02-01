2262aa827c982fa34259dc6523a198cbd1a77390
Steven Hook <steven@hookscode.com>
2020-02-01 15:57:37 -0500

    Add light and manager to scene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,13 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public GameObject lightBulb;
     private Light light;
-    public float lifeIntensity = 1;
+    public float lifeIntensity = 6;
 
     // Start is called before the first frame update
     void Start()
     {
         light = lightBulb.GetComponent<Light>();
-        light.intensity = lifeIntensity;
     }
 
     // Update is called once per frame
@@ -20,7 +19,7 @@ public class GameManager : MonoBehaviour
     {
         if(lifeIntensity >= 0) {
             lifeIntensity -= .001f;
-            light.intensity = lifeIntensity;
+            light.range = lifeIntensity;
         }
     }
 }
