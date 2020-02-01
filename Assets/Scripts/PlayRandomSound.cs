a72687f5c8712834e94d85c82648a4f6534afccd
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2020-02-01 10:09:13 -0500

    Game Object plays random sound clip on spawn

---
+++ Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip[] audioClips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayRandomAudioClip();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayRandomAudioClip()
+    {
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.Play();
+    }
+}
