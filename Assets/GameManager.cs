413c9ee315b5da36cb1346ba4ce43aabd469889e
John Schlanger <john.a.schlanger@gmail.com>
2020-02-01 01:34:01 -0500

    Game manager and dimming light

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject lightBulb;
+    private Light light;
+    public float lifeIntensity = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = lightBulb.GetComponent<Light>();
+        light.intensity = lifeIntensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifeIntensity >= 0) {
+            lifeIntensity -= .001f;
+            light.intensity = lifeIntensity;
+        }
+    }
+}
