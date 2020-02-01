5822100508e0c770d302857b4f9d74f1572202b5
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2020-02-01 12:59:55 -0500

    Created Cable with Collider using 'AttachCableOnCollide' Script

---
+++ Assets/Scripts/AttachCableOnCollide.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachCableOnCollide : MonoBehaviour
+{
+    public string GameObjectName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+     {
+         if (col.gameObject.name == GameObjectName)
+         {
+        // creates joint
+         FixedJoint joint = gameObject.AddComponent<FixedJoint>(); 
+        // sets joint position to point of contact
+         joint.anchor = col.contacts[0].point; 
+        // conects the joint to the other object
+         joint.connectedBody = col.contacts[0].otherCollider.transform.GetComponentInParent<Rigidbody>(); 
+        // Stops objects from continuing to collide and creating more joints
+         joint.enableCollision = false;
+        Debug.Log("Connected!");
+         }
+     }
+}
