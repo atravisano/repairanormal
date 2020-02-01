fcdc03f5ef717a7e94f03b0d31ed06b95e092c3a
John Schlanger <john.a.schlanger@gmail.com>
2020-02-01 16:26:25 -0500

    Syncing light meter and range to end together

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,19 +6,20 @@ public class GameManager : MonoBehaviour
 {
     public GameObject lightBulb;
     private Light light;
-    public float lifeIntensity = 6;
+    public float lifeIntensity = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         light = lightBulb.GetComponent<Light>();
+        lifeIntensity = light.range;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(lifeIntensity >= 0) {
-            lifeIntensity -= .001f;
+            lifeIntensity -= 0.001f;
             light.range = lifeIntensity;
         }
     }
--- Assets/Scaling.cs
+++ Assets/Scaling.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Scaling : MonoBehaviour
 {
-    public GameObject indicator;
-    private Transform indicatorCylinder;
+    public GameObject lightbulb;
+    public Light light;
+    public float initialYScale;
+    public float initialRange;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        lightbulb = GameObject.Find("The Light Bulb");
+        light = lightbulb.GetComponent<Light>();
+        initialYScale = this.transform.localScale.y;
+        initialRange = light.range;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.transform.localScale.y >= 0.0f)
-            this.transform.localScale -= new Vector3(0,.001f,0);
+
+        if(this.transform.localScale.y >= 0.0f) {
+            this.transform.localScale = new Vector3(this.transform.localScale.x, initialYScale * (light.range/initialRange), this.transform.localScale.z);
+        }
     }
 }
