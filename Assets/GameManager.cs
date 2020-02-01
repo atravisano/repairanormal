f86ce9a6a09a4597ba582e6b7b5f62573727c053
drocm <andrewkiproff@gmail.com>
2020-02-01 18:06:24 -0500

    button toggles

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,87 +4,73 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+
+    // THE Light
     public GameObject lightBulb;
-    public GameObject buttonPanel;
     private Light light;
+
+    // Player
     public float lifeIntensity = 1;
-    bool buttonsInSuccessState = true;
+
+    // Button Panel
+    private bool[] buttonState = new bool[6];
+    public GameObject buttonPanel;
+
+
 
     void Awake()
     {
-        Debug.Log("Woke...");
         light = lightBulb.GetComponent<Light>();
     }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Started...");
         light.intensity = lifeIntensity;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if(lifeIntensity >= 0 && !buttonsInSuccessState) {
+        if(lifeIntensity >= 0 && !getPanelButtonState()) {
             lifeIntensity -= .001f;
             light.intensity = lifeIntensity;
         }
-
-        if (Input.GetKeyDown(KeyCode.Space)) {
-
-            Debug.Log("Spaced...");
-            panelButtonPressed();
-
-        }
     }
 
-    public void panelButtonPressed(){
 
-        Debug.Log("panelButtonPressed...YAY");
+    public void panelButtonPressed0(){ buttonState[0] = !buttonState[0]; buttonState[3] = !buttonState[3]; }
+    public void panelButtonPressed1(){ buttonState[1] = !buttonState[1]; buttonState[4] = !buttonState[4]; }
+    public void panelButtonPressed2(){ buttonState[2] = !buttonState[2]; buttonState[5] = !buttonState[5]; }
+    public void panelButtonPressed3(){ buttonState[3] = !buttonState[3]; buttonState[0] = !buttonState[0]; }
+    public void panelButtonPressed4(){ buttonState[4] = !buttonState[4]; buttonState[1] = !buttonState[1]; }
+    public void panelButtonPressed5(){ buttonState[5] = !buttonState[5]; buttonState[5] = !buttonState[5]; }
 
-        Component[] buttons = buttonPanel.GetComponentsInChildren(typeof(Component));
+    /*public void getButtonState0(){ return buttonState[0]; }
+    public void getButtonState1(){ return buttonState[1]; }
+    public void getButtonState2(){ return buttonState[2]; }
+    public void getButtonState3(){ return buttonState[3]; }
+    public void getButtonState4(){ return buttonState[4]; }
+    public void getButtonState5(){ return buttonState[5]; }*/
 
-        int index = 0;
-
-        foreach (Component button in buttons)
-        {   
-
-            //DumpToConsole(button);
-
-        }
-        /*
-            buttons.Add(child.gameObject);
-
-            if( ButtonIsOn ){ 
-                // add to on buttons
-            }
-            
-            if(index % 2 > 0){
-
-                // turn off previous button
-
-            } else {
-
-                // turn off next button
-
-            }
-
-            index++;
-        }
-
-        /*if ( onButtons array includes only the 3 correct buttons ){
-
-
-
+    private bool getPanelButtonState(){
+        if(
+            buttonState[0] == true &&
+            buttonState[1] == true &&
+            buttonState[2] == true &&
+            buttonState[3] == false &&
+            buttonState[4] == false &&
+            buttonState[5] == false
+        ){
+            return true;
         }
         else
         {
-
-
-
-        }*/
-    
+            return false;
+        }
     }
 
 }
---
+++ Assets/Scripts/toggleButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleButton : MonoBehaviour
+{
+
+    public GameObject button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        thisButton = button.GetComponent<GameObject>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // if button is illuminated, shut off illumination else do opposite (thisButton's light intensity = 1ish or 0 ??)
+        
+    }
+
+}
